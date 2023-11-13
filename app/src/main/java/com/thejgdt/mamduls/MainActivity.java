0e8d8a64197dc27dadf6d4e6f99a3b9487197835
ctva <theaberchio@gmail.com>
2023-11-13 19:41:22 +0700

    add clickable drawer icon
    - introduce a clickable drawer icon in the header layout
    - upon clicking the icon, the navigation drawer is triggered to open
    - adjust layout constraints and attributes to ensure proper alignment and behavior

--- app/src/main/java/com/thejgdt/mamduls/MainActivity.java
+++ app/src/main/java/com/thejgdt/mamduls/MainActivity.java
@@ -1,14 +1,24 @@
 package com.thejgdt.mamduls;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        ImageView drawerIcon = findViewById(R.id.drawerIcon);
+
+        drawerIcon.setOnClickListener(view -> drawerLayout.openDrawer(GravityCompat.START));
     }
 }
